16140999f11c66c96a2ddc7860df5db84635d7b9
haraldmarten <haraldmarten.nomper@gmail.com>
2026-09-23 22:34:52 +0300

    Added window and setting.py

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import pygame
+import settings
 
+pygame.init()
+
+SCREEN = pygame.display.set_mode((settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT))
+DESC = pygame.display.set_caption(settings.CAPTION)
+
+running = True
+
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()
+
+pygame.quit()
 
